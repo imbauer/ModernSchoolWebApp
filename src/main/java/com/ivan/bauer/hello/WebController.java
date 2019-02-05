16c62fcec010f3a6bacc5088298f5c2eca5f2634
Ivan <ivan@imbauer.com>
2019-02-05 22:26:51 +0100

    Setup controller endpoint for accessing user information and made login page nice

--- src/main/java/com/ivan/bauer/hello/WebController.java
+++ src/main/java/com/ivan/bauer/hello/WebController.java
@@ -1,10 +1,19 @@
 package com.ivan.bauer.hello;
 
 import org.springframework.stereotype.Controller;
+import java.security.Principal;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
+
+    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @ResponseBody
+    public String currentUserName(Principal principal) {
+        return principal.getName();
+    }
 
     @RequestMapping(value="/")
     public String home(){
